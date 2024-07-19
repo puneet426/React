b0e3e342faa74bef60dcfa32ace17555e585bc4d
Puneet Tiwari <tiwaripuneet426@gmail.com>
2024-07-19 10:52:42 +0530

    01_vite basic app

---
+++ 01_vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+import Chai from "./chai"
+
+function App() {
+  return (
+    // <h1>Chai or React with Vite || Puneet </h1>  
+    // we can return only one function 
+    <div>
+    <Chai/>
+    <h1>chai or react</h1>
+    <p>by hitesh chaudhary</p>
+    </div>
+  )
+}
+export default App
